cbf76d24edd406b762440b1025668bace4c8e61d
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-08-26 10:43:11 -0500

    Added fields: todo: Array<Item>, done: Array<Item>

--- server/models/employee.js
+++ server/models/employee.js
@@ -8,10 +8,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let employeeSchema = new Schema({
+const itemSchema = require('../models/itemSchema');
+
+const employeeSchema = new Schema({
   empId: { type: String, unique: true },
   firstName: { type: String },
-  lastName: { type: String }
+  lastName: { type: String },
+  todo: [ itemSchema ],
+  done: [ itemSchema ]
 }, { collection: 'employees'});
 
 module.exports = mongoose.model('Employee', employeeSchema)
