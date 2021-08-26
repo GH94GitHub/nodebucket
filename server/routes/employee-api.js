5bab908f78aceb2c482e645e4b4fb83b949746cc
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-08-26 10:51:05 -0500

    Added post '/api/employees/:empId/tasks'

--- server/routes/employee-api.js
+++ server/routes/employee-api.js
@@ -37,13 +37,74 @@ router.get('/:empId', async (req, res) => {
       else
       {
         console.log(employee);
-        // Send back the find in JSON
         res.json(employee);
       }
     });
   }
   catch (e)
   {
+    console.log(e);
+    // Send internal server error, with the message
+    res.status(500).send({
+      'message':"Internal server error: " + e.message
+    })
+  }
+});
+
+router.post('/:empId/tasks', async (req, res) => {
+  try {
+    // Construct a task object and fill it
+    const task = {
+      text: req.params.text
+    }
+
+    Employee.findOneAndUpdate({ empId: req.params.empId }, { $push: { todo: task } }, function(err, employee) {
+      // Error
+      if(err) {
+        console.log(err);
+        res.status(500).send({
+          'message': 'MongoDB server error ' + err.message
+        });
+      }
+      // Successful
+      else {
+        console.log(employee);
+        res.json(employee);
+      }
+    })
+  }
+  catch(e) {
+    console.log(e);
+    // Send internal server error, with the message
+    res.status(500).send({
+      'message':"Internal server error: " + e.message
+    })
+  }
+});
+
+router.get('/:empId/tasks', async (req, res) => {
+  try {
+    Employee.findOne({ empId: req.params.empId }, async (err, employee) => {
+      // Error
+      if (err) {
+        console.log(err);
+        res.status(500).send({
+          'message': 'MongoDB server error ' + err.message
+        });
+      }
+      // Successful
+      else {
+        let tasks = {
+          todo: employee.todo,
+          done: employee.done
+        }
+
+        console.log(tasks);
+        res.json(tasks);
+      }
+    })
+  }
+  catch(e) {
     console.log(e);
     // Send internal server error, with the message
     res.status(500).send({
